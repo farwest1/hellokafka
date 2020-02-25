baa1d96084c7dfd54603655c669f1bd3411d9b1c
Bernd <moeller-bernd@gmx.de>
2020-02-25 10:06:26 +0100

    Create a topic with number of partitions (NewTopic Bean)

--- src/main/java/com/bmoellerit/kafka/hellokafka/HellokafkaApplication.java
+++ src/main/java/com/bmoellerit/kafka/hellokafka/HellokafkaApplication.java
@@ -2,6 +2,7 @@ package com.bmoellerit.kafka.hellokafka;
 
 import com.bmoellerit.kafka.hellokafka.integration.Messenger;
 import com.bmoellerit.kafka.hellokafka.service.DoSomethingService;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,11 @@ public class HellokafkaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HellokafkaApplication.class, args);
 	}
+
+	@Bean
+	public NewTopic berndsTopic(){
+	  return new NewTopic("Bernds",2,(short)1);
+  }
 
 }
 
