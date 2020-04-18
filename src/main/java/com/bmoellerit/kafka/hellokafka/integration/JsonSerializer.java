356652d72265463e24a6ecca6b288a5b4aaa3e0c
Bernd <moeller-bernd@gmx.de>
2020-04-18 17:03:30 +0200

    Use (De-)Serializers from the Kafka distribution

--- src/main/java/com/bmoellerit/kafka/hellokafka/integration/JsonSerializer.java
+++ src/main/java/com/bmoellerit/kafka/hellokafka/integration/JsonSerializer.java
@@ -3,6 +3,8 @@ package com.bmoellerit.kafka.hellokafka.integration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
 import org.apache.kafka.common.serialization.Serializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Bernd on 08.02.2020.
@@ -10,6 +12,7 @@ import org.apache.kafka.common.serialization.Serializer;
  * Package com.bmoellerit.kafka.hellokafka.dto
  */
 public class JsonSerializer implements Serializer {
+  private static final Logger LOGGER = LoggerFactory.getLogger(JsonSerializer.class);
 
   @Override
   public void configure(Map configs, boolean isKey) {
@@ -24,12 +27,14 @@ public class JsonSerializer implements Serializer {
   @Override
   public byte[] serialize(String s, Object o) {
     byte[] retVal = null;
+    LOGGER.info("String parameter:" + s);
     ObjectMapper objectMapper = new ObjectMapper();
     try {
       retVal = objectMapper.writeValueAsString(o).getBytes();
     } catch (Exception e) {
       e.printStackTrace();
     }
+    LOGGER.info(retVal.toString());
     return retVal;
   }
 }
